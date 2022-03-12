e6b051c498280207b0d66a1684efa32d7a5b41c7
rbernanda <acappemenang@gmail.com>
2022-03-12 21:57:10 +0700

    add public toml

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
 import { Head } from 'components/Head'
 import { Navbar } from 'components/Navbar'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Head>
         <meta charSet="utf-8" />
         <title>Mini Games</title>
       </Head>
       <Navbar />
       <AppRoutes />
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
